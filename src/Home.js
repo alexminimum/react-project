9ed84f1c825ecef2f7a3ee530f3eb210be2c2493
Alexander Barakhov <alexander@minimum.run>
2022-03-23 23:17:43 +0100

    feat: :construction: testing on click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+  const handleClick = (name, event) => {
+    console.log("Hi " + name, event.target);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={(ev) => handleClick("Manolo", ev)}>Click me</button>
     </div>
   );
 };
